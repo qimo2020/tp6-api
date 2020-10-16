945c3b24d97adcae57e41a4284cbed34dfb8f785
qimo <1718360859@qq.com>
2020-10-16 09:36:54 +0800

    del

--- app/api/model/ApiCode.php
+++
@@ -1,8 +0,0 @@
-<?php declare(strict_types=1);
-namespace app\api\model;
-defined('IN_SYSTEM') or die('Access Denied');
-use think\Model;
-class ApiCode extends Model
-{
-
-}
--- app/api/sql/uninstall.sql
+++
@@ -1,7 +0,0 @@
-DROP TABLE IF EXISTS `pre_api_app`;
-DROP TABLE IF EXISTS `pre_api_controller`;
-DROP TABLE IF EXISTS `pre_api_action`;
-DROP TABLE IF EXISTS `pre_api_param`;
-DROP TABLE IF EXISTS `pre_api_code`;
-DROP TABLE IF EXISTS `pre_api_token`;
-DROP TABLE IF EXISTS `pre_api_member`;
